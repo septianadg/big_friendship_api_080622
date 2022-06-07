3ad77d6e9600baab450e7a1b8eacdf05ab8e3498
septianppm <94352681+septianppm@users.noreply.github.com>
2022-06-08 02:10:12 +0700

    update path.v.2

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	// directory root project go yang kita buat
-	"./models" // memanggil package models pada directory models
-	"./routes"
+	"https://github.com/septianppm/big_friendship_api_080622/tree/main/models" // memanggil package models pada directory models
+	"https://github.com/septianppm/big_friendship_api_080622/tree/main/routes"
 )
 
 func main() {
